1cf4d97377aa8c3c505d292759e4135c7b9e3f3a
Tomas Psotka <psotka439@gmail.com>
2025-02-15 17:29:16 +0100

    Added a header for the site with some styling.
    - Added header folder with header component files
    - Updated the boldness of the "Tomas Psotka" heading
    - Added Bootstrap display-4 class to the heading
    - Created new header component with HTML, CSS, and TypeScript files
    - Modified angular.json, package.json, and package-lock.json to include necessary dependencies
    - Updated app.component.html and app.component.ts to integrate the header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [HeaderComponent]
 })
 export class AppComponent {
   title = 'AngularPortfolioWebsite';
