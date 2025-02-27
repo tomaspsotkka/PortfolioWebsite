4a847bce3a49624e254aa25c88ef45b6f6cd00d5
Tomas Psotka <psotka439@gmail.com>
2025-02-27 17:31:47 +0100

    Fixed typo.
    I fixed the problem where navbar items didn't work properly.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
