293e00c363a95e53f82371179fa1baf67a93c3a8
Tomas Psotka <psotka439@gmail.com>
2025-02-27 11:02:26 +0100

    Added a navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, NavComponent]
 })
 export class AppComponent {
   title = 'AngularPortfolioWebsite';
