1bad7a4e64b0fe8eeb68ba087ab357e641e15b65
Tomas Psotka <psotka439@gmail.com>
2025-03-02 13:39:37 +0100

    I added portfolio cards
    I added:
    - new project-card component
    - displayed cards on protfolio page
    - fields in card: title, body, button, footer with used tool names

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ProjectCardComponent } from '../project-card/project-card.component';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  standalone: true,
+  imports: [ProjectCardComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
