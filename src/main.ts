b06bcdc0014e601236fb034aaa9b8387ad14df55
Tomas Psotka <psotka439@gmail.com>
2025-03-18 23:53:51 +0100

    Fix, forgot to push changes from main.ts file.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    importProvidersFrom(ModalModule.forRoot())
   ]
 }).catch(err => console.error(err));
