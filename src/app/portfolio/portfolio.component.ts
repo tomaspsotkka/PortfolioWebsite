9a87f20b432fa965a762fea473b83b4bd796a5ac
Tomas Psotka <psotka439@gmail.com>
2025-03-25 00:22:21 +0100

    Added filter feature for projects component.
    -you can filter trough projects
    -reset button, which resets all checkboxes at once instead of diselecting it one by one

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -4,11 +4,18 @@ import { Title } from '@angular/platform-browser';
 import { ProjectCardComponent } from '../project-card/project-card.component';
 import { Project } from '../_models/Project';
 import { CommonModule } from '@angular/common';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { Tag } from '../_models/Tag';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [ProjectCardComponent, CommonModule],
+  imports: [
+    ProjectCardComponent,
+    CommonModule,
+    CollapseModule,
+    FormsModule],
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
@@ -16,10 +23,74 @@ export class PortfolioComponent implements OnInit{
   
   projects = {} as Project[];
 
+  isCollapsed: boolean = true;
+  
+  //Languages
+  typescript: boolean = false;
+  python: boolean = false;
+  csharp: boolean = false;
+  java: boolean = false;
+  javascript: boolean = false;
+
+  //Frameworks
+  angular: boolean = false;
+  react: boolean = false;
+  nodejs: boolean = false;
+  aspnet: boolean = false;
+
   constructor(private titleService: Title, private projectsService: ProjectsService){
       this.titleService.setTitle('Tomas Psotka - Portfolio');
     }
   ngOnInit(): void {
+    this.projects = this.projectsService.GetProjects();
+  }
+
+  Filter() {
+    let filterTags: Tag[] = [];
+
+    if (this.typescript) {
+      filterTags.push(Tag.TYPESCRIPT);
+    }
+    if (this.python) {
+      filterTags.push(Tag.PYTHON);
+    }
+    if (this.csharp) {
+      filterTags.push(Tag.CSHARP);
+    }
+    if (this.java) {
+      filterTags.push(Tag.JAVA);
+    }
+    if (this.javascript) {
+      filterTags.push(Tag.JAVASCRIPT);
+    }
+    if (this.angular) {
+      filterTags.push(Tag.ANGULAR);
+    }
+    if (this.react) {
+      filterTags.push(Tag.REACT);
+    }
+    if (this.nodejs) {
+      filterTags.push(Tag.NODEJS);
+    }
+    if (this.aspnet) {
+      filterTags.push(Tag.ASPNET);
+    }
+
+
+    this.projects = this.projectsService.GetProjectsByFilter(filterTags);
+  }
+
+  ResetFilters() {
+    this.typescript = false;
+    this.python = false;
+    this.csharp = false;
+    this.java = false;
+    this.javascript = false;
+    this.angular = false;
+    this.react = false;
+    this.nodejs = false;
+    this.aspnet = false;
+
     this.projects = this.projectsService.GetProjects();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { importProvidersFrom } from '@angular/core';
+import { CollapseModule } from 'ngx-bootstrap/collapse'
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
+    provideAnimations(),
     importProvidersFrom(
       ModalModule.forRoot(),
-      CarouselModule.forRoot())
+      CarouselModule.forRoot(),
+      CollapseModule.forRoot())
   ]
 }).catch(err => console.error(err));
